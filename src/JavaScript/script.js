6c4f74eb5ab047b00be99a8129b5c34a421c6238
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-11-20 14:01:04 +0500

    More btn in the main page was removed from the mobile view

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -14,20 +14,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-function moreFunction() {
-  document.getElementById("myDropdown-more").classList.toggle("show-flex");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn-more')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content-more");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show-flex')) {
-        openDropdown.classList.remove('show-flex');
-      }
-    }
-  }
-}
